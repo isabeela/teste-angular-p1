08198c7d4659cdf3668dace8f9610cd1fa7f8fc0
isabeela <isabeelabg@gmail.com>
2021-09-30 17:09:45 -0300

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import {IsabelawalletComponent} from './isabelawallet/isabelawallet.component';
+
+import { IsabelawalletService } from './isabelawallet.service';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, IsabelawalletComponent],
+  bootstrap: [AppComponent],
+  providers: [IsabelawalletService],
 })
-export class AppModule { }
+export class AppModule {}
